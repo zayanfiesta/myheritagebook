af46f0aa9ebc9bc1dabf9ee4eeeaf35f53f40871
zayanfiesta <zayanfiesta@protonmail.com>
2026-09-13 03:04:35 +0530

    Update HTML title in conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "Total Defense Premium Download & Install Made Easy: Activation, Login, and Already Purchased"
+html_title = "My MyHeritage Login Already Member: How to Access Your MyHeritage Account Online"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
